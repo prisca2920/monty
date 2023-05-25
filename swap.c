6541c5340e9c94e01e0148b235a160634531633f
prisca2920 <prisca2920@gmail.com>
2023-05-25 06:28:44 -0700

    swaps the top two elements of the stack

--- swap.c
+++ swap.c
@@ -8,3 +8,23 @@
  */
 
 void _swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	stack_t *newnode;
+
+	if (!stack || !*stack || !temp->next)
+	{
+	fprintf(stderr "L%u: can't swap, stack too short," line_number);
+	exit(EXIT_FAILURE);
+	}
+
+	*stack = *temp = *newnode;
+	temp = temp->next;
+	temp->prev = NULL;
+
+	newnode->next = temp->next;
+	temp->next = newnode;
+	(temp->next)->prev = temp;
+
+	*stack = temp;
+}
