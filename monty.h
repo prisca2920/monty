1e270c1765f5a6688365b2818dc8223148f140ef
prisca2920 <prisca2920@gmail.com>
2023-05-25 11:04:01 -0700

    header file

--- monty.h
+++ monty.h
@@ -38,6 +38,8 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+void _free(stack_t *head);
+int _atoi(char *str, unsigned int line_number);
 void _push(stack_t **stack, unsigned int line_number);
 void _pall(stack_t **stack, unsigned int line_number);
 void _pint(stack_t **stack, unsigned int line_number);
