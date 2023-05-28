8403f67f883e9091ab4bf8b446c0867da28c3653
prisca2920 <prisca2920@gmail.com>
2023-05-28 02:58:16 -0700

    more free

--- free.c
+++ free.c
@@ -18,3 +18,20 @@ void _free(stack_t *head)
 	}
 	free(head);
 }
+
+/**
+ * _more_free - frees stack, line and file
+ * @stack: the struct
+ * @line: the line to free
+ * @fp: the file to free
+ */
+void _more_free(stack_t *stack, char *line, FILE *fp)
+{
+	if (stack != NULL)
+	{
+	_free(&stack);
+	}
+
+	free(line);
+	fclose(fp);
+}
