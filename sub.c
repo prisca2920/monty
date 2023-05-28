6f5eab144500cbbaa06aa8ad85151e7d47c03672
prisca2920 <prisca2920@gmail.com>
2023-05-28 01:26:21 -0700

    subtracts the top element

---
+++ sub.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * _sub - adds the top two elements of the stack.
+ * @stack: - struct to be checked
+ * @line_number: integer to be checked
+ * Return: 0 success
+ */
+
+void _sub(stack_t **stack, unsigned int line_number)
+{
+	int subtraction, i, j;
+
+	if (!*stack || !stack || !stack->next)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	i = (*stack)->n;
+
+	j = (*stack)->next->n;
+
+	subtraction = j - i;
+
+	pop(stack, line_number);
+
+	(*stack)->n = subtraction;
+}
