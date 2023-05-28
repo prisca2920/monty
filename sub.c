95b3d6cba6558b265db6361134b92528f6927d90
prisca2920 <prisca2920@gmail.com>
2023-05-28 01:28:04 -0700

    subtracts the top element

--- sub.c
+++ sub.c
@@ -13,7 +13,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 
 	if (!*stack || !stack || !stack->next)
 	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
