c1535a0fd2a34df00c8c14fffc887c19829eb6d3
prisca2920 <prisca2920@gmail.com>
2023-05-28 01:31:54 -0700

    divides top/2nd top element

---
+++ div.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * _div - adds the top two elements of the stack.
+ * @stack: - struct to be checked
+ * @line_number: integer to be checked
+ * Return: 0 success
+ */
+
+void _div(stack_t **stack, unsigned int line_number)
+{
+	int division, i, j;
+
+	if (!*stack || !stack || !stack->next)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	i = (*stack)->n;
+
+	j = (*stack)->next->n;
+
+	division = j / i;
+
+	pop(stack, line_number);
+
+	(*stack)->n = division;
+}
