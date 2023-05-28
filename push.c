debae2343783af3cba3d1a8f06e5c38e98d27548
prisca2920 <prisca2920@gmail.com>
2023-05-28 02:28:34 -0700

    pushes an element to stack

--- push.c
+++ push.c
@@ -8,3 +8,38 @@
  */
 
 void _push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *newnode;
+	char *element;
+	int i;
+
+	for (i = 0; element[i] != '\0'; i++)
+	{
+	if (element[0] == '-' && i == 0)
+	continue;
+
+	if (isdigit(element[i]) == 0 || element == NULL)
+	{
+	fprintf(stderr, "L%d: usage: push integer\n", line_number);
+	exit(EXIT_FAILURE);
+	}
+	}
+
+	newnode = malloc(sizeof(stack_t));
+	if (newnode == NULL)
+	{
+	fprintf(stderr, "Error: malloc failed\n");
+	exit(EXIT_FAILURE);
+	}
+
+	newnode->n = atoi(element);
+	newnode->prev = NULL;
+	newnode->next = NULL;
+
+	if (*stack != NULL)
+	{
+	newnode->next = *stack;
+	(*stack)->prev = newnode;
+	}
+	*stack = newnode;
+}
