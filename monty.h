6ee4b179aaf5bdb9c09ef21a85566f978553428e
prisca2920 <prisca2920@gmail.com>
2023-10-19 10:35:03 -0700

    mul

--- monty.h
+++ monty.h
@@ -60,4 +60,5 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void sub(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
 #endif
